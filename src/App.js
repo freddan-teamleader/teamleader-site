9810a7819d4a04d4028384224c3bed5577ef039b
Fredrik Gustafsson <fredrik@teamleader.se>
2020-08-26 15:39:10 +0200

    tete

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>Teamleader</h1>
+      <h1>Teamleader </h1>
       <h2>For sale</h2>
       <h3>
         Contact: <a href="mailto:sales@teamleader.se">sales@teamleader.se</a>
